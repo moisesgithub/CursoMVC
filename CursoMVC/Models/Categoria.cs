ca1e4514aba0381b9d20ec541fe49c39e0b76c4a
moisesgithub <moises.github@outlook.com>
2021-04-26 21:53:22 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Esse campo é obrigatório! ")]
+        public string Descricao { get; set; }
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa! ")]
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ CursosAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CursosAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
